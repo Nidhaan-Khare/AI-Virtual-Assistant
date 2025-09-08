4ffee19e06a7373d2350851ae88180c612de26ae
Nidhaan Khare <nidhaankhare77@gmail.com>
2025-09-08 20:38:43 +0530

    Update index.js
    rendered frontend url added in backend

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ const port=process.env.PORT || 5000;
 // });
 
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://ai-virtual-assistant-0ahx.onrender.com",
     credentials:true
 }));
 
